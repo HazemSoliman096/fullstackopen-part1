65fe3993544efea5bcfd4a2650cfcf2891b23734
dev3 <dev3@DESKTOP-BSHTT23>
2022-10-17 18:22:27 +0200

    1.6: unicafe step1.

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -6,11 +6,28 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const handleGood = () => setGood(good + 1);
+  const handleNeutral = () => setNeutral(neutral + 1);
+  const handleBad = () => setBad(bad + 1);
+
   return (
     <div>
-      code here
+      <Header title={"give feedback"}/>
+      <Btn handle={handleGood} title={"good"} />
+      <Btn handle={handleNeutral} title={"neutral"} />
+      <Btn handle={handleBad} title={"bad"} />
+      <Header title={"statistics"}/>
+      <Stat title={"good"} total={good} />
+      <Stat title={"neutral"} total={neutral} />
+      <Stat title={"bad"} total={bad} />
     </div>
   )
 }
+
+const Header = (props) => <h1>{props.title}</h1>
+
+const Btn = ({handle, title}) => <button onClick={handle}>{title}</button>
+
+const Stat = ({title, total}) => <p>{title} {total}</p>
 
 export default App
