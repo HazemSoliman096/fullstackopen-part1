66a1fb61e63a8fc37d449c45c7f34cc0886a508c
Hazem Abd el-Aziz <hazemsoliman096@gmail.com>
2022-10-18 00:05:20 +0200

    unicafe.

--- Readme.md
+++ Readme.md
@@ -6,3 +6,10 @@
   3. 1.3: course information step3
   4. 1.4: course information step4
   5. 1.5: course information step5
+- unicafe:
+  1. 1.6: unicafe step1.
+  2. 1.7: unicafe step2.
+  3. 1.8: unicafe step3.
+  4. 1.9: unicafe step4.
+  5. 1.10: unicafe step5.
+  6. 1.11*: unicafe step6.
--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -12,12 +12,22 @@ const App = () => {
 
   return (
     <div>
-      <Header title={"give feedback"}/>
-      <Btn handle={handleGood} title={"good"} />
-      <Btn handle={handleNeutral} title={"neutral"} />
-      <Btn handle={handleBad} title={"bad"} />
-      <Header title={"statistics"}/>
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      
+      <Header title={'give feedback'}/>
+      <Btn handle={handleGood} title={'good'} />
+      <Btn handle={handleNeutral} title={'neutral'} />
+      <Btn handle={handleBad} title={'bad'} />
+      <Header title={'statistics'}/>
+      { good + neutral + bad !== 0 &&
+        <table>
+        <Statistics title={'good'} total={good} />
+        <Statistics title={'neutral'} total={neutral} />
+        <Statistics title={'bad'} total={bad} />
+        <Statistics title={'total'} total={good + bad + neutral} />
+        <Statistics title={'average'} total={(good + bad) / (good + bad + neutral)} />
+        <Statistics title={'positive'} total={good / (good + bad + neutral) + '%'} />
+      </table>
+} {good + neutral + bad === 0 && <p>no feedback given</p>}
     </div>
   )
 }
@@ -26,23 +36,6 @@ const Header = (props) => <h1>{props.title}</h1>
 
 const Btn = ({handle, title}) => <button onClick={handle}>{title}</button>
 
-const Statistics  = ({good, neutral, bad}) => {
-  if(good === 0 && neutral === 0 && bad === 0) {
-    return (
-      <p>No feedback given</p>
-    );
-  } else {
-    return (
-      <>
-        <p>good {good}</p>
-        <p>neutral {neutral}</p>
-        <p>bad {bad}</p>
-        <p>all {good + neutral + bad}</p>
-        <p>average {(good - bad) / (good + neutral + bad)}</p>
-        <p>positive {good / (good + neutral + bad)}</p>
-      </>
-    );
-  }
-}
+const Statistics  = ({title, total}) => <tr><td>{title} {total}</td></tr>
 
 export default App
